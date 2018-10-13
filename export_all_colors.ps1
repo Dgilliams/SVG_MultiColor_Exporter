18b6dc12d3c381fe35b73eaa31802dabd6b1120f
Damien Gilliams <uffoward@gmail.com>
2018-10-13 10:49:27 -0700

    uploaded script and sample svg files

---
+++ export_all_colors.ps1
@@ -0,0 +1,17 @@
+$colorToReplace = 'ed2027'
+$colors = @("000000","4E3521","8C3B28","B28E28","F4EA6E","F0E6FF","4D22D2","8E2ABE","3596EC","0ECF7C")
+
+$files = Get-ChildItem -Path $PSScriptRoot
+
+Write-Output $files
+
+foreach($file in $files) {
+  if(-NOT ($file.BaseName -eq 'export_all_colors')) {
+    foreach ($color in $colors) {
+
+    $newFileName = $file.Basename + "_" + $color + ".svg"
+    Write-Output $newFileName
+    powershell -Command "(gc *.svg) -replace '$colorToReplace', '$color' | Out-File $newFileName.svg"
+    }
+  }
+}
