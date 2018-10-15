a322ffe000a166805eb1910aa2356d0fd2ff3bf0
Damien Gilliams <uffoward@gmail.com>
2018-10-15 13:20:58 -0700

    windows support for python script
    on testing on windows, print methods required parentheses

--- color_exporter.py
+++ color_exporter.py
@@ -1,9 +1,9 @@
 import os
 import fileinput
 
-#authored by Damien Gilliams of DamosDesigns.com on 10/15/2018
+# authored by Damien Gilliams of www.DamosDesigns.com on 10/15/2018
 
-print "\n\n~~~Start of Program~~~\n"
+print ("\n\n~~~Start of Program~~~\n")
 
 originalColor = "ed2027"
 newColors = ["000000", "4E3521", "8C3B28", "B28E28", "F4EA6E", "F0E6FF", "4D22D2", "8E2ABE", "3596EC", "0ECF7C", "ED2027"]
@@ -20,7 +20,7 @@ for root, dirs, files in os.walk("."):
 
             # make sure color exists in file
             if svgData.find(originalColor) == -1:
-                print "ERROR: Change fill color of " + filename + " to #ED2027 for script to work"
+                print ("ERROR: Change fill color of " + filename + " to #ED2027 for script to work")
                 break
 
             for newColor in newColors:
@@ -32,8 +32,8 @@ for root, dirs, files in os.walk("."):
                 newFile.write(newSvgFileData)
                 newFile.close
 
-                print newFileName
+                print (newFileName)
 
         # output replaced results to new file
 
-print "\n\n~~~Done!~~~\n"
+print ("\n\n~~~Done!~~~\n")
